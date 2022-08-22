eb0bbe9c04e93b56a10d1f6ceb0f05dbecefb23a
raymondfam <raymondfam@gmail.com>
2022-08-22 10:00:19 -0500

    add goerli and mumbai networks

--- hardhat.config.js
+++ hardhat.config.js
@@ -10,6 +10,7 @@ require("dotenv").config()
 
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "Your etherscan API key"
 const POLYGONSCAN_API_KEY = process.env.POLYGONSCAN_API_KEY || "Your polygonscan API key"
+const MUMBAI_RPC_URL = process.env.MUMBAI_RPC_URL
 const FORKING_BLOCK_NUMBER = process.env.FORKING_BLOCK_NUMBER
 const MAINNET_RPC_URL =
     process.env.MAINNET_RPC_URL ||
@@ -19,63 +20,84 @@ const RINKEBY_RPC_URL =
     process.env.RINKEBY_RPC_URL || "https://eth-rinkeby.alchemyapi.io/v2/your-api-key"
 const PRIVATE_KEY = process.env.PRIVATE_KEY
 const REPORT_GAS = process.env.REPORT_GAS || false
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL || ""
 
 module.exports = {
-  networks: {
-      hardhat: {
-          // If you want to do some forking set `enabled` to true
-          forking: {
-              url: MAINNET_RPC_URL,
-              blockNumber: FORKING_BLOCK_NUMBER,
-              enabled: false,
-          },
-          chainId: 31337,
-      },
-      localhost: {
-          chainId: 31337,
-      },
-      rinkeby: {
-          url: RINKEBY_RPC_URL,
-          accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
-          //   accounts: {
-          //     mnemonic: MNEMONIC,
-          //   },
-          saveDeployments: true,
-          chainId: 4,
-      },
-  },
-  etherscan: {
-      // yarn hardhat verify --network <NETWORK> <CONTRACT_ADDRESS> <CONSTRUCTOR_PARAMETERS>
-      apiKey: {
-          rinkeby: ETHERSCAN_API_KEY,
-          kovan: ETHERSCAN_API_KEY,
-          polygon: POLYGONSCAN_API_KEY,
-      },
-  },
-  gasReporter: {
-      enabled: REPORT_GAS,
-      currency: "USD",
-      outputFile: "gas-report.txt",
-      noColors: true,
-      // coinmarketcap: process.env.COINMARKETCAP_API_KEY,
-  },
-  namedAccounts: {
-      deployer: {
-          default: 0, // here this will by default take the first account as deployer
-          1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
-      },
-  },
-  solidity: {
-      compilers: [
-          {
-              version: "0.8.4",
-          },
-          {
-              version: "0.8.7",
-          },
-      ],
-  },
-  mocha: {
-      timeout: 200000, // 200 seconds max for running tests
-  },
+    networks: {
+        hardhat: {
+            // If you want to do some forking set `enabled` to true
+            forking: {
+                url: MAINNET_RPC_URL,
+                blockNumber: FORKING_BLOCK_NUMBER,
+                enabled: false,
+            },
+            chainId: 31337,
+        },
+        localhost: {
+            chainId: 31337,
+        },
+        rinkeby: {
+            url: RINKEBY_RPC_URL,
+            accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+            //   accounts: {
+            //     mnemonic: MNEMONIC,
+            //   },
+            saveDeployments: true,
+            chainId: 4,
+        },
+        goerli: {
+            url: GOERLI_RPC_URL,
+            accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+            //   accounts: {
+            //     mnemonic: MNEMONIC,
+            //   },
+            saveDeployments: true,
+            chainId: 5,
+        },
+        mumbai: {
+            url: MUMBAI_RPC_URL,
+            accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+            //   accounts: {
+            //     mnemonic: MNEMONIC,
+            //   },
+            saveDeployments: true,
+            chainId: 80001,
+        },
+    },
+    etherscan: {
+        // yarn hardhat verify --network <NETWORK> <CONTRACT_ADDRESS> <CONSTRUCTOR_PARAMETERS>
+        apiKey: {
+            rinkeby: ETHERSCAN_API_KEY,
+            kovan: ETHERSCAN_API_KEY,
+            polygon: POLYGONSCAN_API_KEY,
+            goerli: ETHERSCAN_API_KEY,
+            polygonMumbai: POLYGONSCAN_API_KEY,
+        },
+    },
+    gasReporter: {
+        enabled: REPORT_GAS,
+        currency: "USD",
+        outputFile: "gas-report.txt",
+        noColors: true,
+        // coinmarketcap: process.env.COINMARKETCAP_API_KEY,
+    },
+    namedAccounts: {
+        deployer: {
+            default: 0, // here this will by default take the first account as deployer
+            1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
+        },
+    },
+    solidity: {
+        compilers: [
+            {
+                version: "0.8.4",
+            },
+            {
+                version: "0.8.7",
+            },
+        ],
+    },
+    mocha: {
+        timeout: 200000, // 200 seconds max for running tests
+    },
 }
--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -9,6 +9,14 @@ const networkConfig = {
         name: "rinkeby",
         weth: "0xc778417E063141139Fce010982780140Aa0cD5Ab",
     },
+    5: {
+        name: "goerli",
+        weth: "0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6",
+    },
+    80001: {
+        name: "mumbai",
+        weth: "0xA6FA4fB5f76172d178d61B04b0ecd319C5d1C0aa",
+    },
     1: {
         name: "mainnet",
     },
